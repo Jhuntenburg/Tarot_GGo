a29ba11beb7fd662da5553d6c788e77fa0ba397a
Joe <Jhuntenburg@gmail.com>
2023-08-18 15:24:52 -0400

    new repo

---
+++ src/main/java/rocks/zipcode/tarot_ggo/TarotGGoApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.tarot_ggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotGGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarotGGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rocks/zipcode/tarot_ggo/TarotGGoApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.tarot_ggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotGGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
